bc54b5ef065d53a4917e3a042409448cc66068bc
Sebastian <sebastian.kuhlgert@gmail.comsebastian.kuhlgert@gmail.comsebastian.kuhlgert@gmail.com>
2023-10-04 14:31:48 -0400

    add MongoDB to download

--- src/server/routes/download.js
+++ src/server/routes/download.js
@@ -3,6 +3,9 @@ const { Parser } = require('@json2csv/plainjs');
 const db = require('../db/sqlite');
 var router = express.Router();
 
+const Data = require('../db/models/data');
+const Device = require('../db/models/device');
+
 var dayjs = require('dayjs');
 var timezone = require('dayjs/plugin/timezone');
 var utc = require('dayjs/plugin/utc');
@@ -23,6 +26,7 @@ router.get('/', (req, res) => {
 
 const DEVICE_ID = process.env.DEVICE_ID;
 const DEVICE_TIMEZONE = process.env.DEVICE_TIMEZONE;
+const DB_TYPE = process.env.SQLITE_FILE? "SQLITE" : process.env.MONGO_CONNECTION_STRING? "MONGODB" : null || "SQLITE"
 
 const unitConverter = require('../models/convert.js');
 
@@ -53,42 +57,104 @@ router.get('/:date', async (req, res) => {
         var end_time = dayjs(selectedDate).endOf('year').tz(DEVICE_TIMEZONE).toISOString();
     }    
 
+    
     var q = `SELECT * from data WHERE data.device_id = "${DEVICE_ID}" AND datetime(created_at) BETWEEN datetime('${start_time}') AND datetime('${end_time}') ORDER BY ROWID ASC`;
+    
+    if(DB_TYPE == "MONGODB")
+        Device.findOne({default: true})
+        .then(device => {
+            // Find all entries for the default device
+            const deviceID = device.device_id;
+            const device_timezone = device.timezone;
 
-    await db.all(q, function (err, rows) {
-        if (err)
-            res.status(500).send();
-        else {
-            rows = rows.map( row => {
-                for(let key in row){
-                    if(req.query.units && req.query.units === 'imperial'){
-                        let converted = unitConverter(row[key], key, 'i')
-                        let new_key = key.replace(/\[(C|hPa)\]/gi, function (x) {
-                            if(x.toLowerCase() === '[c]')
-                                return '[F]'
-                            if(x.toLowerCase() === '[hpa]')
-                                return '[inHg]'
-                            return x;
-                        });
-                        if(key !== new_key){
-                            row[new_key] = converted[0]
-                            delete row[key]
+            Data.aggregate([
+                    {
+                        $match: { 
+                            device_id: deviceID, 
+                            created_at: { $gte: new Date(start_time), $lte: new Date(end_time) } 
                         }
-                        else
-                            row[key] = converted[0]
+                    },
+                    { $addFields: { Time: '$created_at' } },
+                    { $project: { created_at: 0, _id: 0, __v: 0, device_id: 0 } }
+                ])
+                .then(rows => {
+                    rows = rows.map( row => {
+                    for(let key in row){
+                        if(req.query.units && req.query.units === 'imperial'){
+                            let converted = unitConverter(row[key], key, 'i')
+                            let new_key = key.replace(/\[(C|hPa)\]/gi, function (x) {
+                                if(x.toLowerCase() === '[c]')
+                                    return '[F]'
+                                if(x.toLowerCase() === '[hpa]')
+                                    return '[inHg]'
+                                return x;
+                            });
+                            if(key !== new_key){
+                                row[new_key] = converted[0]
+                                delete row[key]
+                            }
+                            else
+                                row[key] = converted[0]
+                        }
+                        row['Time'] = dayjs(row['Time']).tz(device_timezone).format();
+                        return row;
                     }
-                    let created_at_offset = dayjs(row['created_at']).tz(DEVICE_TIMEZONE).format('ZZ')
-                    row['created_at'] = row['created_at'].replace('Z',created_at_offset);
-                    return row;
-                }
+                });
+                let json2csv = new Parser();
+                let csv = rows.length == 0? "" : json2csv.parse(rows);
+                res.header('Content-Type', 'text/csv');
+                res.attachment(`Weather-Data ${selectedDate.replace('/', '-')}.csv`);
+                res.send(csv);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: `Query Error`,
+                    errorMessage: String(err).replace(/Error:\s?/i,' ')
+                }); 
             });
-            let json2csv = new Parser();
-            let csv = rows.length == 0? "" : json2csv.parse(rows);
-            res.header('Content-Type', 'text/csv');
-            res.attachment(`Weather-Data ${selectedDate.replace('/', '-')}.csv`);
-            res.send(csv);
-        }
-    });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: `Query Error`,
+                errorMessage: String(err).replace(/Error:\s?/i,' ')
+            }); 
+        });
+
+    if(DB_TYPE == "SQLITE")
+        db.all(q, function (err, rows) {
+            if (err)
+                res.status(500).send();
+            else {
+                rows = rows.map( row => {
+                    for(let key in row){
+                        if(req.query.units && req.query.units === 'imperial'){
+                            let converted = unitConverter(row[key], key, 'i')
+                            let new_key = key.replace(/\[(C|hPa)\]/gi, function (x) {
+                                if(x.toLowerCase() === '[c]')
+                                    return '[F]'
+                                if(x.toLowerCase() === '[hpa]')
+                                    return '[inHg]'
+                                return x;
+                            });
+                            if(key !== new_key){
+                                row[new_key] = converted[0]
+                                delete row[key]
+                            }
+                            else
+                                row[key] = converted[0]
+                        }
+                        let created_at_offset = dayjs(row['created_at']).tz(DEVICE_TIMEZONE).format('ZZ')
+                        row['created_at'] = row['created_at'].replace('Z',created_at_offset);
+                        return row;
+                    }
+                });
+                let json2csv = new Parser();
+                let csv = rows.length == 0? "" : json2csv.parse(rows);
+                res.header('Content-Type', 'text/csv');
+                res.attachment(`Weather-Data ${selectedDate.replace('/', '-')}.csv`);
+                res.send(csv);
+            }
+        });
 
 });
 
