d781de206f2911d9bbd1a0d93b2d59fa08abdba8
Sebastian <sebastian.kuhlgert@gmail.comsebastian.kuhlgert@gmail.comsebastian.kuhlgert@gmail.com>
2023-10-16 17:18:39 -0400

    update 404 page for dark theme

--- src/client/pages/NotFound.js
+++ src/client/pages/NotFound.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,7 +6,29 @@ import Col from 'react-bootstrap/Col';
 
 import {Link} from 'react-router-dom'
 
+import { SettingsContext } from '../context/settingsContext'
+
+
 export default function NotFound(props) {
+
+  const [state] = useContext(SettingsContext);
+
+  useEffect( () => {
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      let theme = window.matchMedia('(prefers-color-scheme: dark)').matches? 'dark': 'light'
+      document.body.setAttribute('data-bs-theme', theme);
+    })
+
+    let theme = state.theme;
+
+    if(state.theme === 'auto')  
+      theme = window.matchMedia('(prefers-color-scheme: dark)').matches? 'dark': 'light';
+    
+    document.body.setAttribute('data-bs-theme', theme);
+
+  }, [state.theme])
+  
   return (
     <main>
       <Container>
@@ -14,10 +36,10 @@ export default function NotFound(props) {
           <Col className="p-3">
             <h3 className="mb-4">
               <i className="bi-bug-fill"></i> Page not found
-            </h3><Link className="btn btn-light" to="/">Back to Today's Weather</Link>
+            </h3><Link className="btn btn-outline-info" to="/">Back to Today's Weather</Link>
           </Col>
           <Col className="col-md-8 col-sm-12 p-3">
-            <img className="img-fluid" src="/images/404.svg" alt="404 error" />
+            <img className="img-fluid" id="img-404" src="/images/404.svg" alt="404 error" />
           </Col>
         </Row>
       </Container>
