b8ae9d2d5df33aed565c20e8546ce9305e2ab16f
Sebastian <sebastian.kuhlgert@gmail.com>
2023-10-29 13:16:03 -0400

    update footer

--- src/client/components/Main/FooterContainer.js
+++ src/client/components/Main/FooterContainer.js
@@ -10,7 +10,9 @@ export default function FooterContainer(props) {
       <Container>
         <Row className="px-1 py-2">
           <Col sm className="text-end text-muted small">
-            Weather Station | <a href="https://github.com/sebasstian23/Server-WeatherStation" rel="noreferrer" target="_blank">GitHub</a>
+            Sources: <a href="https://github.com/sebasstian23/ESP32-WeatherStation" rel="noreferrer" target="_blank">Weather Station</a>
+            {" | "}
+            <a href="https://github.com/sebasstian23/Server-WeatherStation" rel="noreferrer" target="_blank">Application</a>
           </Col>
         </Row>
       </Container>
