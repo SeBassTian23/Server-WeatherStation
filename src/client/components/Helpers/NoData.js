36206cd3995c0d1ffb9690c8b5352e926c3d0a6f
Sebastian <sebastian.kuhlgert@gmail.com>
2024-07-23 15:34:09 +0200

    Update UI no data summary

--- src/client/components/Helpers/NoData.js
+++ src/client/components/Helpers/NoData.js
@@ -6,9 +6,9 @@ import Col from 'react-bootstrap/Col';
 export default function NoData() {
   return (
     <Row>
-      <Col className='text-center'>
+      <Col className='text-center mb-5'>
         <i className='bi-activity text-muted opacity-25' style={{fontSize: '12em'}} />
-        <p className='fw-light'>No Data Available</p>
+        <p className='fw-light text-muted'>No Data Available</p>
       </Col>
     </Row>
   )
--- src/client/components/Panels/Summary.js
+++ src/client/components/Panels/Summary.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 
 import { LabelUnitStrip } from '../../helpers/label-format';
 
@@ -10,8 +10,8 @@ import Table from 'react-bootstrap/Table'
 import NoData from '../Helpers/NoData';
 import Placeholder from 'react-bootstrap/Placeholder';
 
-import {SettingsContext} from '../../context/settingsContext'
-import {unitConverter} from '../../helpers/convert'
+import { SettingsContext } from '../../context/settingsContext'
+import { unitConverter } from '../../helpers/convert'
 
 import downloadLink from '../../helpers/download-link';
 
@@ -29,32 +29,32 @@ export default function Summary(props) {
 
   const [summary, setSummary] = useState({});
 
-  useEffect( () => {
+  useEffect(() => {
     setSummary(summary => {
-      return {...summary, ...props}
+      return { ...summary, ...props }
     });
-  }, [props] )
+  }, [props])
 
-  if(props.isLoading || !summary.selectedDate){
+  if (props.isLoading || !summary.selectedDate) {
     return <>
       <Card>
         <Card.Body>
           <Card.Title className='text-info'>Summary</Card.Title>
           <Placeholder animation='glow'>
             <Placeholder sm={5} className='mb-4 fw-light' />
-            <Row xs={{cols: 1}} xl={{cols: 2}} >
+            <Row xs={{ cols: 1 }} xl={{ cols: 2 }} >
               <Col className='table-responsive'>
                 <TableBody period={props.period} />
-                {[...new Array(12)].map( (e, idx) => {
-                  return <Placeholder sm={12} key={idx} bg={ idx % 2 ? 'secondary' : null} size={'lg'} className='mb-2' />
-                  })
+                {[...new Array(12)].map((e, idx) => {
+                  return <Placeholder sm={12} key={idx} bg={idx % 2 ? 'secondary' : null} size={'lg'} className='mb-2' />
+                })
                 }
               </Col>
               <Col className='table-responsive'>
                 <TableBody period={props.period} />
-                {[...new Array(12)].map( (e, idx) => {
-                  return <Placeholder sm={12} key={idx} bg={ idx % 2 ? 'secondary' : null} size={'lg'} className='mb-2' />
-                  })
+                {[...new Array(12)].map((e, idx) => {
+                  return <Placeholder sm={12} key={idx} bg={idx % 2 ? 'secondary' : null} size={'lg'} className='mb-2' />
+                })
                 }
               </Col>
             </Row>
@@ -70,29 +70,31 @@ export default function Summary(props) {
 function SummaryTable(props) {
 
   const [state] = useContext(SettingsContext);
-  const period = (props.period === 'now')? 'today' : `the period of ${ (props.period === 'range' && props.selectedDate)? dayjs.duration( dayjs(props.selectedDate[1]).diff(dayjs(props.selectedDate[0]), 'day') , "days").humanize() : `a ${props.period}`}`
+  const [downloadCSVLink, setDownloadCSVLink] = useState(null);
+  const period = (props.period === 'now') ? 'today' : `the period of ${(props.period === 'range' && props.selectedDate) ? dayjs.duration(dayjs(props.selectedDate[1]).diff(dayjs(props.selectedDate[0]), 'day'), "days").humanize() : `a ${props.period}`}`
   const left = props.tableLeft || []
-  const right =  props.tableRight || []
-  const download = downloadLink(props.period, props.selectedDate, state.units) || '#'
+  const right = props.tableRight || []
+
+  useEffect(() => {
+    if (left.length > 0 || right.length > 0)
+      setDownloadCSVLink(downloadLink(props.period, props.selectedDate, state.units) || null);
+    else
+      setDownloadCSVLink(null);
+  }, [props.selectedDate])
 
   return (
     <Card>
       <Card.Body>
-        <a className='float-end me-2' href={download} aria-label="Download Latest Observations as CSV"><i className='bi bi-download text-muted' /> </a>
+        {downloadCSVLink && <a className='float-end me-2' href={downloadCSVLink} aria-label="Download Latest Observations as CSV"><i className='bi bi-download text-muted' /></a>}
         <Card.Title className='text-info'>Summary</Card.Title>
-        <Card.Subtitle className='text-muted mb-4 fw-light'>Data summarized for {period}.
-        {(!props.device.active && period === 'today') && <>
-          <span style={{fontSize: '0.875rem'}} className='float-end px-1 py-0 alert alert-warning'><i className='bi bi-wifi-off' /> Offline</span>
-        </>}
-        </Card.Subtitle>
+        <Card.Subtitle className='text-muted mb-4 fw-light'>Data summarized for {period}.</Card.Subtitle>
         <SummaryTableContent left={left} right={right} period={props.period} />
-        {(props.sunrise && left.length > 0) &&
-        <ul className='list-unstyled mb-0 text-muted fw-light small columns-4'>
+        <ul className='list-unstyled mb-0 text-muted fw-light small' style={{ columnGap: "0.75rem", gridTemplateColumns: "repeat(4, 1fr)", display: "grid" }}>
           <li><i className='bi-sunrise-fill text-info' /> Sunrise: {dayjs(timezoneAdjust(props.sunrise, props.timezone || 'UTC')).format('LT')} (local)</li>
           <li><i className='bi-sunset-fill text-info' /> Sunset: {dayjs(timezoneAdjust(props.sunset, props.timezone || 'UTC')).format('LT')} (local)</li>
-          <li><i className='bi-sun-fill text-info' /> Daylight: {dayjs.duration( dayjs(props.sunset).diff(dayjs(props.sunrise), 'minute') , "minute").format('HH:mm')} hrs</li>
+          <li><i className='bi-sun-fill text-info' /> Daylight: {dayjs.duration(dayjs(props.sunset).diff(dayjs(props.sunrise), 'minute'), "minute").format('HH:mm')} hrs</li>
           <li><i className='bi-moon-stars-fill text-info' /> Moon: {props.lunarphase}</li>
-        </ul>}
+        </ul>
       </Card.Body>
     </Card>
   )
@@ -101,9 +103,9 @@ function SummaryTable(props) {
 function SummaryTableContent(props) {
   const left = props.left || []
   const right = props.right || []
-  if(left.length > 0 & right.length > 0){
+  if (left.length > 0 & right.length > 0) {
     return (
-      <Row xs={{cols: 1}} xl={{cols: 2}} >
+      <Row xs={{ cols: 1 }} xl={{ cols: 2 }} >
         <Col>
           <TableBody rows={left} period={props.period} />
         </Col>
@@ -113,14 +115,14 @@ function SummaryTableContent(props) {
       </Row>
     )
   }
-  else{
-    return(
+  else {
+    return (
       <NoData />
     )
   }
 }
 
-function TableBody ( props ) {
+function TableBody(props) {
 
   const rows = props.rows || []
 
@@ -128,27 +130,27 @@ function TableBody ( props ) {
     <Table className='table-sm ws-obs-tbl' striped >
       <thead className='thead-light small text-center'>
         <tr>
-          <th className='text-start' style={{width: '45%'}}>Parameter</th>
-          <th className='text-center'>{props.period === 'now'? 'Current' : 'Average'}</th>
+          <th className='text-start' style={{ width: '45%' }}>Parameter</th>
+          <th className='text-center'>{props.period === 'now' ? 'Current' : 'Average'}</th>
           <th className='text-end'>Low</th>
           <th className='text-center'><small>→</small></th>
           <th className='text-start'>High</th>
         </tr>
       </thead>
       <tbody className='text-center small text-nowrap'>
-        {rows.map( (row,idx) => (
-            <TableRow {...row} key={idx} />
-          )
+        {rows.map((row, idx) => (
+          <TableRow {...row} key={idx} />
+        )
         )}
       </tbody>
     </Table>
   )
 }
 
-function TableRow ( props ) {
+function TableRow(props) {
   const [state] = useContext(SettingsContext);
   let unit = ""
-  if( props.unit && props.unit !== "" ){
+  if (props.unit && props.unit !== "") {
     unit = <small className="text-muted">[{unitConverter(1, props.unit, state.units)[1]}]</small>
   }
 
@@ -156,8 +158,8 @@ function TableRow ( props ) {
     <tr>
       <td className='text-start'>{LabelUnitStrip(props.header)} <small className='text-muted'>{unit}</small></td>
       <td className='text-center'>
-      {props.trend && <><small className='text-info'>{props.trend}</small> {' '}</> }
-        <ValueFormat value={props.latest? props.latest : props.avg} title={props.title} unit={props.header} />
+        {props.trend && <><small className='text-info'>{props.trend}</small> {' '}</>}
+        <ValueFormat value={props.latest ? props.latest : props.avg} title={props.title} unit={props.header} />
       </td>
       <td className='text-end'>
         <ValueFormat value={props.min} title={props.title} unit={props.header} />
@@ -175,12 +177,11 @@ function ValueFormat(props) {
   //(class=`index ${addClass}`) #{value}] #[small #[small.text-muted #{unit}]]
   let value = unitConverter(props.value, props.unit, state.units)[0]
   value = value > 999 ? value.toFixed(0).toLocaleString() : value.toLocaleString()
-  if( props.addClass )
-    return(
-      <span className={props.addClass} title={props.title ? props.title: null}>{value}</span>
+  if (props.addClass)
+    return (
+      <span className={props.addClass} title={props.title ? props.title : null}>{value}</span>
     )
   return (
     value
   )
 }
-    
