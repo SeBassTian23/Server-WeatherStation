eac6ffae6a081e9010917affc1d7b0aecd22ca85
Sebastian <sebastian.kuhlgert@gmail.com>
2024-07-23 15:35:05 +0200

    Update offline indicator

--- src/client/pages/View.js
+++ src/client/pages/View.js
@@ -89,7 +89,7 @@ export default function View(props) {
         <SubHeader isLoading={loading} {...data.subheader} {...data.datetime}  {...data.station} />
         <Row className='align-items-top pb-4'>
           <Col sm>
-            <Summary isLoading={loading} {...data.summary} {...data.datetime} {...data.station} />
+            <Summary isLoading={loading} {...data.summary} {...data.datetime} />
           </Col>
           <Col as='aside' lg='4' className='pt-4 pt-lg-0 d-print-none'>
             <TabsContextProvider>
